cee8c8ca31d434389278c6967cec3374013d2636
debugged-llc <mbuboltz@debugged.app>
2020-05-30 03:19:13 -0500

    chime for stock fcw
    chime for stock fcw

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -85,7 +85,7 @@ ALERTS = [
       "BRAKE!",
       "Risk of Collision",
       AlertStatus.critical, AlertSize.full,
-      Priority.HIGHEST, VisualAlert.fcw, AudibleAlert.none, 1., 2., 2.),  # no EON chime for stock FCW
+      Priority.HIGHEST, VisualAlert.fcw, AudibleAlert.chimeWarningRepeat, 1., 2., 2.),  #EON chime for stock FCW
 
   Alert(
       "steerSaturated",
